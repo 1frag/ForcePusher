4a3b952c1b83b0bc0b0b1daf87dac8bde9432dc6
Alexey Piskunov <piskunov.alesha@gmail.com>
2021-09-11 22:36:28 +0700

    add getting config (#5)

--- Sources/PythonForcePusher/force_push.py
+++ Sources/PythonForcePusher/force_push.py
@@ -1,20 +1,46 @@
+import json
 import os
 import sys
 
 from github import Github
 
+fields_config = ['token', 'owner', 'repository', 'login']
+
+
+def get_config():
+    config = {}
+    gh_proc = os.popen('gh api user')
+    for source in (os.environ['HOME'] + '/.push.json', gh_proc.name):
+        try:
+            config.update(json.loads(open(source).read()))
+        except:
+            pass
+    try:
+        owner, repository = os.popen('git config --get remote.origin.url')\
+            .read() \
+            .removeprefix('git@github.com:') \
+            .removesuffix('.git') \
+            .split('/')
+        config['owner'], config['repository'] = owner, repository
+    except:
+        pass
+    return {k: config.get(k) for k in fields_config}
+
 
 def _main():
-    token = input('Token: ')
-    owner = input('Owner: ')
-    repository = input('Repository: ')
+    config = get_config()
+    token = config['token'] or input('Token: ')
+    owner = config['owner'] or input('Owner: ')
+    repository = config['repository'] or input('Repository: ')
+    assignee = config['login'] or input('Assignee: ')
     no_signed = '--no-signed' in sys.argv
+    pr_number = int(input('PR number: '))
 
     g = Github(token)
     repo = g.get_repo('/'.join([owner, repository]))
-    pr = repo.get_pull(int(input('Номер PR: ')))
+    pr = repo.get_pull(pr_number)
 
-    if not (pr.assignee and pr.assignee.login == input('Assignee: ')):
+    if not (pr.assignee and pr.assignee.login == assignee):
         print("is the pull request yours?")
         exit(1)
 
